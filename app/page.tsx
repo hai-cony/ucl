9ce22679db5ad3cdcd3ad3edb2594f71a5f024f8
fauzyramadan <fauzyfauzy288@gmail.com>
2025-12-15 18:29:50 +0700

    change: landing page

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,16 @@
 import { Button } from "@/components/ui/button";
 import {
   Briefcase,
+  Calculator,
   LayoutDashboard,
+  NotebookIcon,
+  NotebookPen,
+  NotebookPenIcon,
   Package,
   ShoppingCart,
 } from "lucide-react";
 import { useRouter } from "next/navigation";
+import InventoryTopbar from "./inventory/topbar";
 
 export default function Home() {
   // inisiasi router jang navigasi halaman / url
@@ -22,22 +27,22 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <div className="flex justify-evenly flex-wrap space-x-2 w-full">
+        <div className="grid grid-cols-3 gap-6 w-full max-w-3xl mx-auto">
           <Button
             // kirim parameter module ka function handleModuleClick
             onClick={() => handleModuleClick("inventory")}
             variant={"outline"}
-            className="h-20 block"
+            className="h-32 block"
           >
             <div className="flex justify-center">
-              <Briefcase />
+              <Briefcase/>s
             </div>
             <div>Inventory</div>
           </Button>
           <Button
             onClick={() => handleModuleClick("dashboard")}
             variant={"outline"}
-            className="h-20 block"
+            className="h-32 block"
           >
             <div className="flex justify-center">
               <LayoutDashboard />
@@ -47,7 +52,7 @@ export default function Home() {
           <Button
             onClick={() => handleModuleClick("purchasing")}
             variant={"outline"}
-            className="h-20 block"
+            className="h-32 block"
           >
             <div className="flex justify-center">
               <ShoppingCart />
@@ -57,12 +62,32 @@ export default function Home() {
           <Button
             onClick={() => handleModuleClick("product")}
             variant={"outline"}
-            className="h-20 block"
+            className="h-32 block"
           >
             <div className="flex justify-center">
               <Package />
             </div>
-            <div>Product</div>
+            <div>Production</div>
+          </Button>
+          <Button
+            onClick={() => handleModuleClick("planner")}
+            variant={"outline"}
+            className="h-32 block"
+          >
+            <div className="flex justify-center">
+              <NotebookPen />
+            </div>
+            <div>Planner</div>
+          </Button>
+          <Button
+            onClick={() => handleModuleClick("accounting")}
+            variant={"outline"}
+            className="h-32 block"
+          >
+            <div className="flex justify-center">
+              <Calculator />
+            </div>
+            <div>Accounting</div>
           </Button>
         </div>
       </main>
